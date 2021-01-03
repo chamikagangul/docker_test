4d727a7458d763b148c5a21a4a8987c51a229435
chamika <pchamikagangul@gmail.com>
2021-01-03 21:30:08 +0530

    counting

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+count = 0
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+  setInterval(()=>{
+    count =count+1
+    console.log(count)
+  },5000);
+  res.render('index', { count: count });
 });
 
 module.exports = router;
